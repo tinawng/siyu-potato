c9671ac0b2c667d05b51710908bb92ea618f2890
Tina Wang <mwow@yopmail.com>
2020-07-27 00:46:42 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const spawn = require("child_process").spawn;
+
+const app = express()
+
+app.listen(3000, () => {
+  console.log("serv on 3k")
+})
+
+app.get("/url", (req, res) => {
+  var regex = /temp=([^'C]+)/;
+  var cmd = spawn("/opt/vc/bin/vcgencmd", ["measure_temp"]);
+  cmd.stdout.on("data", function (buf) {
+    var obj = parseFloat(regex.exec(buf.toString("utf8"))[1]);
+    res.json(obj);
+  });
+});
+
+app.get("/cmd/:cmd", (req, res) => {
+  let params = req.params.cmd.split(' ')
+  let cmd = spawn(params[0], params.slice(1))
+
+  cmd.stdout.on("data", function (buf) {
+    res.json(buf.toString("utf8"));
+  });
+});
+
+app.get("/cmd", (req, res) => {
+  let params = req.query.cmd.split(' ')
+  let cmd = spawn(params[0], params.slice(1))
+
+  cmd.stdout.on("data", function (buf) {
+    res.json(buf.toString("utf8"));
+  });
+})
