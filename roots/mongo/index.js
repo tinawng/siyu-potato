7d24bbe82447914fd110ea55ca36c0e5c4cb3c3e
Tina Wang <mwow@yopmail.com>
2020-08-13 23:42:37 +0200

    🚨 Use recommended param

--- roots/mongo/index.js
+++ roots/mongo/index.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 export default function (app) {
-    mongoose.connect('mongodb://127.0.0.1/garden', { useNewUrlParser: true });
+    mongoose.connect('mongodb://127.0.0.1/garden', { useNewUrlParser: true, useUnifiedTopology: true });
 
     const db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error:'));
