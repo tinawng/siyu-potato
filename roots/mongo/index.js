3cc675ac74f5ba19b79b7b2c07763f0b2eb3b753
Tina Wang <tinawang@laptop>
2020-08-14 17:05:09 +0200

    ♻️ rename env var

--- roots/mongo/index.js
+++ roots/mongo/index.js
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
 
 export default function (app) {
-    if (!process.env.MONGO_HOST) return;
-    mongoose.connect('mongodb://' + process.env.MONGO_HOST + '/garden', { useNewUrlParser: true, useUnifiedTopology: true });
+    if (!process.env.MONGODB_HOST) return;
+    mongoose.connect('mongodb://' + process.env.MONGODB_HOST + '/garden', { useNewUrlParser: true, useUnifiedTopology: true });
 
     const db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error:'));
