d53d2c0ccce85f8c3e89566523a8ad54485987a6
Tina Wang <mwow@yopmail.com>
2020-09-07 23:20:56 +0200

    ✨ Add events logging into database

---
+++ logger/index.js
@@ -0,0 +1,23 @@
+import log_model from './models/Log.js';
+
+export default function (reponse, payload) {
+  let log_obj = {
+    method: reponse.request.raw.method,
+    url: reponse.request.raw.url,
+    params: reponse.request.params,
+    ip: reponse.request.headers['x-forwarded-for'],
+    user_id: reponse.request.user_id || undefined,
+    status: reponse.raw.statusCode,
+    request_body_size: JSON.stringify(reponse.request.body).length,
+    response_body_size: payload.length,
+    error: null
+  }
+
+  const parsed_payload = JSON.parse(payload);
+
+  if (parsed_payload.error)
+    log_obj.error = parsed_payload.error;
+
+  const log = new log_model(log_obj);
+  log.save();
+}
---
+++ logger/models/Log.js
@@ -0,0 +1,23 @@
+// https://mongoosejs.com/docs/guide.html
+
+import mongoose from 'mongoose';
+import { db_logs } from '../../mongoose.config.js';
+
+const Schema = mongoose.Schema;
+
+let log_schema = new Schema({
+  method: String,
+  url: String,
+  params: Schema.Types.Mixed,
+  ip: String,
+  user_id: String,
+  status_code: Number,
+  request_body_size: Number,
+  response_body_size: Number,
+},
+  {
+    collection: 'records'
+  }
+)
+
+export default db_logs.model('Log', log_schema);
