ca3829208376e228214fa55bad75f8100622d73d
Tina Wang <mwow@yopmail.com>
2020-09-26 01:01:44 +0200

    ✨ Firebase storage managment endpoints

--- roots/firebase/storage/index.js
+++ roots/firebase/storage/index.js
@@ -1,4 +1,5 @@
 import admin from 'firebase-admin';
+import fs from 'fs';
 
 export default async function (app, opts) {
 
@@ -7,7 +8,39 @@ export default async function (app, opts) {
         credential: admin.credential.cert(serviceAccount),
         storageBucket: "gs://" + serviceAccount.project_id + ".appspot.com"
     });
-    
-    var fire_bucket = admin.storage().bucket();
-    // fire_bucket.upload("./test.txt"); // this is working 🎉
+
+    var bucket = admin.storage().bucket();
+    // bucket.upload("./test.txt"); // this is working 🎉
+
+    app.post("/uploadlocal", async (req, res) => {
+        if (req.body.secret === process.env.SECRET) {
+            await bucket.upload(req.body.path);
+            if (req.body.cleanup)
+                fs.unlinkSync(req.body.path);
+            res.code(200).send();
+        }
+        else
+            res.code(401).send();
+
+    });
+
+    app.post("/rm", async (req, res) => {
+        if (req.body.secret === process.env.SECRET) {
+            await bucket.file(req.body.path).delete();
+            res.code(200).send();
+        }
+        else
+            res.code(401).send();
+    })
+
+    app.post("/makepublic", async (req, res) => {
+        if (req.body.secret === process.env.SECRET) {
+            const file = bucket.file(req.body.path)
+            file.makePublic(function (err, api_res) {
+                res.code(200).send("https://www.googleapis.com/download/storage/v1/b/" + serviceAccount.project_id + ".appspot.com/o/" + api_res.object + "?generation=" + api_res.generation + "&alt=media");
+            });
+        }
+        else
+            res.code(401).send();
+    })
 }
