b7e1c865dfd2efd1ec038b385216510549402ccc
Tina Wang <tinawang@laptop>
2020-12-25 15:50:49 +0100

    🗑️ Cleanup endpoint

--- roots/cmd/index.js
+++ roots/cmd/index.js
@@ -1,26 +1,18 @@
 import { spawn } from 'child_process';
 
 export default async function (app, opts) {
-    app.get("/temp", (req, res) => {
-        var regex = /temp=([^'C]+)/;
-        var cmd = spawn("/opt/vc/bin/vcgencmd", ["measure_temp"]);
-        cmd.stdout.on("data", function (buf) {
-            var obj = parseFloat(regex.exec(buf.toString("utf8"))[1]);
-            res.send(obj);
-        });
-    });
 
     app.get("/:cmd", (req, res) => {
-        let params = req.params.cmd.split(' ')
+        let params = req.params.cmd.split(' ');
         let cmd = spawn(params[0], params.slice(1))
 
-        cmd.stdout.on("data", function (buf) {  
+        cmd.stdout.on("data", function (buf) {
             res.send(buf.toString("utf8"));
         });
     });
 
     app.post("/", (req, res) => {
-        let params = req.query.cmd.split(' ')
+        let params = req.body.cmd.split(' ')
         let cmd = spawn(params[0], params.slice(1))
 
         cmd.stdout.on("data", function (buf) {
